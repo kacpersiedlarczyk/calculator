d80fe2930c9e63895d47a8e3c0e29df0fbb18372
kacpersiedlarczyk <100533998+kacpersiedlarczyk@users.noreply.github.com>
2023-10-04 16:26:15 +0200

    add functions for all of the basic math operations

---
+++ script.js
@@ -0,0 +1,19 @@
+let operator;
+let currentNumber;
+let previousNumber;
+
+function add(a, b) {
+    return a + b;
+};
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}

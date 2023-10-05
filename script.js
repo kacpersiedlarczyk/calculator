c079ea986d910ba0d6e3b43d0b593bef497e88e6
kacpersiedlarczyk <100533998+kacpersiedlarczyk@users.noreply.github.com>
2023-10-05 18:45:27 +0200

    add display functionality

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-let operator;
+let currentOperator;
 let currentOperand;
 let previousOperand;
+let displayNumber = "0";
 
 function add(a, b) {
     return a + b;
@@ -32,3 +33,59 @@ function operate(operator, a, b) {
             console.log("Oops! Something went wrong.");
     };
 };
+
+function updateDisplayNumber(number) {
+    if (number === "." && displayNumber.split("").includes(".")) {
+        return;
+    };
+
+    if (number === "0" && displayNumber === "0") {
+        return;
+    };
+    
+    if (displayNumber === "0") {
+        displayNumber = number;
+    } else {
+        displayNumber = displayNumber + number;
+    };
+};
+
+function updateDisplay() {
+    document.querySelector(".output").innerText = displayNumber;
+};
+
+function updateOperator(operator) {
+    currentOperator = operator;
+};
+
+function updatePreviousOperand() {
+    previousOperand = Number(displayNumber);
+    displayNumber = "0";
+};
+
+// function updateCurrentOperand() {
+
+// };
+
+
+
+
+
+
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]')
+const equalsButton = document.querySelector('[data-equals]')
+
+numberButtons.forEach(button => {
+    button.addEventListener("click", event => {
+        updateDisplayNumber(event.target.innerText);
+        updateDisplay();
+    });
+});
+
+operationButtons.forEach(button => {
+    button.addEventListener("click", event => {
+        updateOperator(event.target.innerText);
+        updatePreviousOperand();
+    });
+});
